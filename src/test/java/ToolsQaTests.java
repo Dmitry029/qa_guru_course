93748f3d5c97198be30f5a27597d5b0e003713e4
Dmitry Kostjuchenko <kdp.029029@gmail.com>
2024-06-20 00:19:23 +0300

    new commit

--- src/test/java/ToolsQaTests.java
+++ src/test/java/ToolsQaTests.java
@@ -70,5 +70,5 @@ public class ToolsQaTests {
         $("#dateOfBirthInput").sendKeys(date);
         $("#dateOfBirthInput").sendKeys(Keys.ENTER);
     }
-    // for demoqa
+    // for demoqa new
 }
